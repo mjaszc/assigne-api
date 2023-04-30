82a5f477f672c92b36bb1f0ec01f87e61d29f879
jasmar2 <111734497+jasmar2@users.noreply.github.com>
2023-04-30 18:38:10 +0200

    Created endpoint for getting project by id (#4)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from typing import List
 
 import schemas
-import crud
+import project_crud as project_crud
 from database import SessionLocal
 
 
@@ -27,12 +27,20 @@ async def root():
 
 @app.post("/projects/", response_model=schemas.Project)
 async def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
-    return crud.create_project(db=db, project=project)
+    return project_crud.create_project(db=db, project=project)
+
+
+@app.get("/projects/{id}", response_model=schemas.Project)
+async def get_project_by_id(id: int, db: Session = Depends(get_db)):
+    project = project_crud.get_project_by_id(db=db, project_id=id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return project
 
 
 @app.get("/projects/", response_model=List[schemas.Project])
 async def get_all_projects(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
-    projects = crud.get_all_projects(db, skip=skip, limit=limit)
+    projects = project_crud.get_all_projects(db, skip=skip, limit=limit)
     return projects
--- app/project_crud.py
+++ app/project_crud.py
@@ -12,5 +12,9 @@ def create_project(db: Session, project: schemas.ProjectCreate):
     return db_item
 
 
+def get_project_by_id(db: Session, project_id: int):
+    return db.query(models.Project).filter(models.Project.id == project_id).first()
+
+
 def get_all_projects(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Project).offset(skip).limit(limit).all()
