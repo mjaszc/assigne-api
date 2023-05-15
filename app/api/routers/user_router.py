16e5972f78bdd79c121871937ef22c8cd8f9a910
jasmar2 <111734497+jasmar2@users.noreply.github.com>
2023-05-15 19:32:50 +0200

    Created get all users endpoint and moved register endpoint (#17)

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 import app.schemas.user_schema as user_schema
 import app.crud.user_crud as user_crud
 from app.db.database import SessionLocal
 
+from typing import List
 
-router = APIRouter(prefix="/api/v1")
+
+router = APIRouter(prefix="/api/v1/users")
 
 
 def get_db():
@@ -17,15 +19,12 @@ def get_db():
         db.close()
 
 
-@router.post("/register", response_model=user_schema.User)
-def register(user: user_schema.UserCreate, db: Session = Depends(get_db)):
-    # Check if user already exists
-    existing_user = user_crud.get_user(db, user.email, user.username)
-    if existing_user:
-        raise HTTPException(
-            status_code=400,
-            detail="User with this email or username already registered",
-        )
-
-    db_user = user_crud.create_user(db, user)
-    return db_user
+@router.get("/", response_model=List[user_schema.User])
+async def get_all_users(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+    current_user: user_schema.User = Depends(user_crud.get_current_user),
+):
+    users = user_crud.get_all_users(db, skip, limit)
+    return users
