f49d092a3a3a97f63572c6e2be31ee0c261c27c7
jasmar2 <111734497+jasmar2@users.noreply.github.com>
2023-05-16 08:48:47 +0200

    Added the get user by id endpoint (#18)

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,11 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import app.schemas.user_schema as user_schema
 import app.crud.user_crud as user_crud
 from app.db.database import SessionLocal
-
-from typing import List
 
 
 router = APIRouter(prefix="/api/v1/users")
@@ -19,7 +17,7 @@ def get_db():
         db.close()
 
 
-@router.get("/", response_model=List[user_schema.User])
+@router.get("/", response_model=user_schema.User)
 async def get_all_users(
     skip: int = 0,
     limit: int = 100,
@@ -28,3 +26,15 @@ async def get_all_users(
 ):
     users = user_crud.get_all_users(db, skip, limit)
     return users
+
+
+@router.get("/{id}", response_model=user_schema.User)
+async def get_project_by_id(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: user_schema.User = Depends(user_crud.get_current_user),
+):
+    user = user_crud.get_user_by_id(db, id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
