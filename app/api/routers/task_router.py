5712330ae87930ff06fabe14ca36cbb224df318e
jasmar2 <jaszczaninmarceli@gmail.com>
2023-06-06 19:12:13 +0200

    Deleted tasks assigned to the project endpoint

--- app/api/routers/task_router.py
+++ app/api/routers/task_router.py
@@ -7,8 +7,6 @@ from app.db.database import SessionLocal
 import app.schemas.user_schema as user_schema
 import app.crud.user_crud as user_crud
 
-from typing import List
-
 router = APIRouter(prefix="/api/v1/projects/{project_id}/tasks")
 
 def get_db():
@@ -27,13 +25,3 @@ async def create_task_for_the_project(
     current_user: user_schema.User = Depends(user_crud.get_current_user),
 ):
     return task_crud.create_task(db, task, project_id)
-
-
-@router.get("/", response_model=List[task_schema.ProjectTasks])
-def get_assigned_tasks_for_the_project(
-    project_id: int,
-    db: Session = Depends(get_db),
-    current_user: user_schema.User = Depends(user_crud.get_current_user),
-):
-    assigned_tasks = task_crud.get_assigned_tasks(db, project_id)
-    return assigned_tasks
