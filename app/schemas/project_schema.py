8f4bc9ff5bf473af469beca02d29f451786f34bd
jasmar2 <jaszczaninmarceli@gmail.com>
2023-06-06 19:10:18 +0200

    Get project by id endpoint now display assigned tasks

--- app/schemas/project_schema.py
+++ app/schemas/project_schema.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import date
 from app.schemas.user_schema import User
-
+from typing import List
+from app.schemas.task_schema import Task
 
 class ProjectBase(BaseModel):
     name: str
     description: Optional[str]
-    start_date: date
+
 
 
 class ProjectCreate(ProjectBase):
@@ -20,7 +21,9 @@ class ProjectUpdate(ProjectBase):
 
 class Project(ProjectBase):
     id: int
+    start_date: date
     author: User
+    assigned_tasks: Optional[List[Task]] = []
 
     class Config:
         orm_mode = True
