c7cb50c951abfc05c735f5520d88811c2ebb89a5
jasmar2 <jaszczaninmarceli@gmail.com>
2023-06-12 19:45:57 +0200

    Wrote function for assign user to task endpoint

--- app/crud/task_crud.py
+++ app/crud/task_crud.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 import app.models.task_model as task_model
 import app.schemas.task_schema as task_schema
 import app.schemas.project_schema as project_schema
+import app.models.user_model as user_model
 
 
 def create_task(db: Session, task: task_schema.TaskCreate, current_project: project_schema.Project):
@@ -43,5 +45,18 @@ def delete_task(db: Session, task_id: int, project_id: int):
     if not db_task:
         return False
     db.delete(db_task)
+    db.commit()
+    return True
+
+def assign_user_to_task(db: Session, task_id: int, project_id: int, user_id: int):
+    db_task = db.query(task_model.Task).filter(task_model.Task.id == task_id, task_model.Task.project_id == project_id).first()
+    if not db_task:
+        raise HTTPException(status_code=404, detail=f"Task with ID {task_id} not found")
+
+    user = db.query(user_model.User).filter(user_model.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+
+    db_task.assigned_user.append(user)
     db.commit()
     return True
