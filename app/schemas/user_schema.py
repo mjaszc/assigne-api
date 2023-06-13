5ba74652b062fae8317ec32b1b70cf3aca9f435e
Marceli Jaszczanin <jaszczaninmarceli@gmail.com>
2023-06-13 18:12:29 +0200

    Assigned tasks now are displayed in user info (#38)

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import Optional, List
+
+from app.schemas.task_schema import Task
 
 # USER
 class UserBase(BaseModel):
@@ -18,6 +20,7 @@ class UserCreate(UserBase):
 class User(UserBase):
     is_active: Optional[bool]
     id: int
+    assigned_tasks: Optional[List[Task]] = []
 
     class Config:
         orm_mode = True
