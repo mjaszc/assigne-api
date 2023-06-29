9911d89c0e427d24f7e38c0aad0a01b826c2d528
jasmar2 <jaszczaninmarceli@gmail.com>
2023-06-29 08:34:31 +0200

    Added unit test for update task db

--- app/tests/test_task_database.py
+++ app/tests/test_task_database.py
@@ -2,6 +2,7 @@ import app.schemas.task_schema as task_schema
 import app.crud.task_crud as task_crud
 import app.models.project_model as project_model
 import app.models.user_model as user_model
+import app.models.task_model as task_model
 
 # CRUD DATABASE OPERATIONS FOR TASK 
 
@@ -59,3 +60,40 @@ def test_get_task_by_id(session):
     get_task = task_crud.get_task_by_id(session, created_task.id, test_project.id)
     assert get_task.title == created_task.title
     assert get_task.description == created_task.description
+
+def test_update_task(session):
+    test_project = project_model.Project(
+        id=1,
+        name="Test Project",
+        description="This is a test project.",
+        start_date="2023-06-20",
+        author=user_model.User(
+            id=1,
+            email="user@example.com",
+            username="string",
+            is_active=True,
+            assigned_tasks=[]
+        ),
+        assigned_users=[]
+    )
+
+    session.add(test_project)
+    session.commit()
+
+    test_task = task_model.Task(
+        id = 1,
+        title = "test_task",
+        description = "test_description",
+        project_id = 1
+    )
+
+    session.add(test_task)
+    session.commit()
+
+    updated_task_data = {"title": "Updated Task Name", "description": "This is the updated task description."}
+    updated_task = task_schema.TaskBase(**updated_task_data)
+    task_crud.update_task(session, test_task.id , updated_task, test_project.id)
+
+    session.refresh(test_task)
+    assert test_task.title == "Updated Task Name"
+    assert test_task.description == "This is the updated task description."
