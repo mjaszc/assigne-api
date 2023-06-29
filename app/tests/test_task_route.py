91eede05928361e42095a7d8df7fce9cbc40c7d7
jasmar2 <jaszczaninmarceli@gmail.com>
2023-06-29 20:02:28 +0200

    Added unit test for get task by id endpoint

--- app/tests/test_task_route.py
+++ app/tests/test_task_route.py
@@ -66,3 +66,61 @@ def test_create_task(client):
         "description": "test_desc",
         "id": 1,
     }
+
+def test_get_task(client):
+    project_data = {
+        "name": "test_proj",
+        "description": "test_desc"
+    }
+
+    mock_create_project = MagicMock(return_value=project_schema.Project(
+        id=1,
+        name=project_data["name"],
+        description=project_data["description"],
+        start_date="2023-06-20",
+        author=user_schema.User(
+            id=1,
+            email="user@example.com",
+            username="string",
+            is_active=True,
+            assigned_tasks=[]
+        ),
+        assigned_tasks=[]
+    ))
+    project_crud.create_project = mock_create_project
+
+    response = client.post("/api/v1/projects", json=project_data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id": 1,
+        "name": "test_proj",
+        "description": "test_desc",
+        "start_date": "2023-06-20",
+        "author": {
+            "id": 1,
+            "email": "user@example.com",
+            "username": "string",
+            "is_active": True,
+            "assigned_tasks": []
+        },
+        "assigned_tasks": []
+    }
+
+    mock_get_task = MagicMock(return_value=task_schema.Task(
+        id=1,
+        title="test_task",
+        description="test_desc"
+    ))
+
+    task_crud.get_task_by_id = mock_get_task
+
+    response = client.get("/api/v1/projects/1/tasks/1")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "title": "test_task",
+        "description": "test_desc",
+        "id": 1,
+    }
+
