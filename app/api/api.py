af662d16bee9d54d0fc7fd15c78198a4430da42e
jasmar2 <jaszczaninmarceli@gmail.com>
2023-07-18 13:35:08 +0200

    Moved the discussion section to the separate files

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.routers import project_router, user_router, task_router
+from app.api.routers import project_router, user_router, task_router, discussion_router
 
 api_router = APIRouter()
 
 api_router.include_router(project_router.router, tags=["projects"])
 api_router.include_router(task_router.router, tags=["tasks"])
+api_router.include_router(discussion_router.router, tags=["task discussions"])
 api_router.include_router(user_router.router, tags=["users"])
